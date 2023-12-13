52a958f3fd692882f277d5a1c97c1c643d495e7a
LionsTech <lionstech@vivaldi.net>
2023-12-13 19:48:12 +0100

    Add PAM rule for swaylock

--- modules/system.nix
+++ modules/system.nix
@@ -20,6 +20,21 @@
     sudo.execWheelOnly = true;
     # allow wayland lockers to unlock the screen
     pam.services.gtklock.text = "auth include login";
+    pam.services.swaylock.text = ''
+      # Account management.
+      account required pam_unix.so
+
+      # Authentication management.
+      auth sufficient pam_unix.so   likeauth try_first_pass
+      auth required pam_deny.so
+
+      # Password management.
+      password sufficient pam_unix.so nullok sha512
+
+      # Session management.
+      session required pam_env.so conffile=/etc/pam/environment readenv=0
+      session required pam_unix.so
+    '';
 
     # userland niceness
     rtkit.enable = true;
